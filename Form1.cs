e191a7885d6d2ce037421a4af6e560400a5a5436
csdaemon <86532783+csdaemon@users.noreply.github.com>
2021-12-16 10:26:13 -0500

    Added Source & Project Files
    Added Source & Project Files

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScratchPad
+{
+    public partial class ScratchPad : Form
+    {
+        public ScratchPad()
+        {
+            InitializeComponent();
+            this.Icon = Properties.Resources._8155_200_tSM_icon;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
